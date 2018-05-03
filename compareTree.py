2daab33d1605b59d43dd4e95a3ca4e7d271cdf40
Tom Hauser <tom.hauser@gmail.com>
2018-05-03 07:00:51 -0400

    Pretty large refactoring of the code.
    Found some issues in the way FileType is determined. I think there's probably
    a better way to figure it out; I'd start with a check using the basename
    itself rather than the whole path, because that would allow us to
    differentiate between files like *LayerData.bin and *TerrainData.bin

--- compareTree.py
+++ compareTree.py
@@ -1,211 +1,140 @@
-import os, glob, re, csv, datetime # Imports python modules
+import os
+import glob
+import re
+import csv
+import datetime  # Imports python modules
+import createTree
+from configparser import ConfigParser
 from datetime import tzinfo
 
-cwd = os.path.dirname(os.path.realpath(__file__)) # Grabs current working directory
-cwd = cwd + '\BattleTech_Data\StreamingAssets\data\**\**' # Appends data path plus wildcards to current directory
+file_type_map = {
+    "\\Prefabs\\": "Prefab",
+    "\\Moods\\": "MoodSettings",
+    "\\Sprites\\": "Sprite",
+    "\\player\\": "Sprite",
+    "\\UnlockedAssets\\": "Sprite",
+    "\\SVGs\\": "SVGAsset",
+    "\\Swatches\\": "ColorSwatch",
+    "\\UIModules\\": "UIModulePrefabs",
+    "\\abilities\\": "AbilityDef",
+    "\\ammunition\\": "AmmunitionDef",
+    "\\ammunitionBox\\": "AmmunitionBoxDef",
+    "\\audioevents": "AudioEventDef",
+    "\\backgrounds\\": "BackgroundDef",
+    "\\behaviorVariables\\": "BehaviorVariableScope",
+    "\\buildings\\": "BuildingDef",
+    "\\cast\\": "CastDef",
+    "\\chassis\\": "ChassisDef",
+    "\\constants\\": "ContractOverride",
+    "\\conversationBuckets\\": "DialogBucketDef",
+    "\\descriptions\\": "BaseDescriptionDef",
+    "\\designMasks\\": "DesignMaskDef",
+    "\\dropship\\": "DropshipDef",
+    "\\events\\": "SimGameEventDef",
+    # need exception for .json files here or it will break with Texture2D
+    "\\factions\\": "FactionDef",
+    "\\genderedoptions\\": "GenderedOptionsListDef",
+    "\\hardpoints\\": "HardpointDataDef",
+    "\\heatsinks\\": "HeatSinkDef",
+    "\\jumpjets\\": "JumpJetDef",
+    "\\lance\\": "LanceDef",
+    "\\lifepathNode\\": "LifepathNodeDef",
+    "\\mech\\": "MechDef",
+    "\\mechlabincludes\\": "MechLabIncludeDef",
+    "\\milestones\\": "SimGameMilestonDef",
+    "\\movement\\": "MovementCapabilitiesDef",
+    "\\nameLists\\": "SimGameStringList",
+    "\\pathing\\": "PathingCapabilitiesDef",
+    "\\pilot\\": "PilotDef",
+    "\\portraits\\": "PortraitSettings",
+    "\\shipUpgrades\\": "ShipModuleUpgrade",
+    "\\shops\\": "ShopDef",
+    "\\simGameConstants\\": "SimGameConstants",
+    "\\simGameConversations\\": "SimGameConversations",  # .convo.bytes files
+    "\\simGameStatDesc\\": "SimGameStatDescDef",
+    "\\starsystem\\": "StarSystemDef",
+    "\\turretChassis\\": "TurretChassisDef",
+    "\\turrets\\": "TurretDef",
+    "\\upgrades\\": "UpgradeDef",
+    "\\vehicle\\": "VehicleDef",
+    "\\vehicleChassis\\": "VehicleChassisDef",
+    "\\weapon\\": "WeaponDef",
+    "\\factions\\": "Texture2D",  # need exception for .dds files here or it will break with FactionDef. Also needs to add line twice for Texture2D and Sprite if in emblems/player/ folder
+    "\\assetbundles\\": "AssetBundle",
+    "\\maps\\": "LayerData",  # ...LayerData.bin files
+    "\\maps2\\": "TerrainData"  # ...TerrainData.bin files
+}
 
-defaultTree = open("defaultTree.txt", "r")
+"""
+returns the FileType from the FileType map!
+"""
+def get_file_type(file_path):
+    for key, value in file_type_map.items():
+        if key in file_path:
+            return value
+    return "ERROR: TYPE NOT DEFINED!"
 
-defaultFiles = defaultTree.read().split('\n')
 
-vmo = open("defaultManifest.csv")
-lines = vmo.readlines()
-vmo.close()
+"""
+Copies the contents of defaultManifest.csv into our new VersionManifest.csv, in preparation for 
+appending the added files to it
+"""
+def copy_original_manifest():
+    original_version_manifest = open("defaultManifest.csv", 'r')
+    lines = original_version_manifest.readlines()
+    original_version_manifest.close()
 
-vmw = open("VersionManifest.csv","w")
-vmw.writelines([item for item in lines[:-2]])
-vmw.close()
+    new_version_manifest = open("VersionManifest.csv", "w")
+    # omit last two lines of original, since we'll be appending to it
+    new_version_manifest.writelines([item for item in lines[:-2]])
+    new_version_manifest.close()
 
-currentFiles = glob.glob (cwd, recursive=True) # Creates file tree
-currentFiles = [re.sub(r'.*StreamingAssets\\', '', eachfile) for eachfile in currentFiles] # Removes folder path up to data\
-currentFiles = filter(None, currentFiles) #Removes blanks lines
 
-addedFiles = set(defaultFiles) ^ set(currentFiles)
-addedFiles = filter(None, addedFiles) #Removes blanks lines
+"""
+returns the list of added files that don't appear in a default installation
+"""
+def get_added_file_list():
+    default_tree = open("defaultTree.txt", "r")
+    default_files = default_tree.read().split('\n')
+    default_tree.close()
 
-for x in addedFiles: 
-    fileID = os.path.basename(x)
-    fileID = os.path.splitext(fileID)[0]
-    fileType = "ERROR:TYPE NOT DEFINED!"
-    while True:
-        if "\\Prefabs\\" in x: 
-            fileType = "Prefab" 
-            break
-        if "\\Moods\\" in x: 
-            fileType = "MoodSettings" 
-            break
-        if "\\Sprites\\" in x: 
-            fileType = "Sprite" 
-            break #Need exception for .dds, .png
-        if "\\player\\" in x: 
-            fileType = "Sprite" 
-            break #Need .dds exception
-        if "\\UnlockedAssets\\" in x: 
-            fileType = "Sprite" 
-            break #Need .png exception, add twice for Texture2D
-        if "\\SVGs\\" in x: 
-            fileType = "SVGAsset" 
-            break
-        if "\\Swatches\\" in x: 
-            fileType = "ColorSwatch" 
-            break
-        if "\\UIModules\\" in x: 
-            fileType = "UIModulePrefabs" 
-            break
-        if "\\abilities\\" in x: 
-            fileType = "AbilityDef" 
-            break
-        if "\\ammunition\\" in x: 
-            fileType = "AmmunitionDef" 
-            break
-        if "\\ammunitionBox\\" in x: 
-            fileType = "AmmunitionBoxDef" 
-            break
-        if "\\audioevents\\" in x: 
-            fileType = "AudioEventDef" 
-            break
-        if "\\backgrounds\\" in x: 
-            fileType = "BackgroundDef" 
-            break
-        if "\\behaviorVariables\\" in x: 
-            fileType = "BehaviorVariableScope" 
-            break
-        if "\\buildings\\" in x: 
-            fileType = "BuildingDef" 
-            break
-        if "\\cast\\" in x: 
-            fileType = "CastDef" 
-            break
-        if "\\chassis\\" in x: 
-            fileType = "ChassisDef" 
-            break
-        if "\\constants\\" in x: 
-            fileType = "ApplicationConstants" 
-            break
-        if "\\contracts\\" in x: 
-            fileType = "ContractOverride" 
-            break
-        if "\\conversationBuckets\\" in x: 
-            fileType = "DialogBucketDef" 
-            break
-        if "\\conversations\\" in x: 
-            fileType = "ConversationContent" 
-            break
-        if "\\descriptions\\" in x: 
-            fileType = "BaseDescriptionDef" 
-            break
-        if "\\designMasks\\" in x: 
-            fileType = "DesignMaskDef" 
-            break
-        if "\\dropship\\" in x: 
-            fileType = "DropshipDef" 
-            break
-        if "\\events\\" in x: 
-            fileType = "SimGameEventDef" 
-            break
-        if "\\factions\\" in x: 
-            fileType = "FactionDef" 
-            break #need exception for .json files here or it will break with Texture2D
-        if "\\genderedoptions\\" in x: 
-            fileType = "GenderedOptionsListDef" 
-            break
-        if "\\hardpoints\\" in x: 
-            fileType = "HardpointDataDef" 
-            break
-        if "\\heatsinks\\" in x: 
-            fileType = "HeatSinkDef" 
-            break
-        if "\\heraldry\\" in x: 
-            fileType = "HeraldryDef" 
-            break
-        if "\\jumpjets\\" in x: 
-            fileType = "JumpJetDef" 
-            break
-        if "\\lance\\" in x: 
-            fileType = "LanceDef" 
-            break
-        if "\\lifepathNode\\" in x: 
-            fileType = "LifepathNodeDef" 
-            break
-        if "\\mech\\" in x: 
-            fileType = "MechDef" 
-            break
-        if "\\mechlabincludes\\" in x: 
-            fileType = "MechLabIncludeDef" 
-            break
-        if "\\milestones\\" in x: 
-            fileType = "SimGameMilestoneDef" 
-            break # .json files
-        if "\\movement\\" in x: 
-            fileType = "MovementCapabilitiesDef" 
-            break
-        if "\\nameLists\\" in x: 
-            fileType = "SimGameStringList" 
-            break
-        if "\\pathing\\" in x: 
-            fileType = "PathingCapabilitiesDef" 
-            break
-        if "\\pilot\\" in x: 
-            fileType = "PilotDef" 
-            break
-        if "\\portraits\\" in x: 
-            fileType = "PortraitSettings" 
-            break
-        if "\\shipUpgrades\\" in x: 
-            fileType = "ShipModuleUpgrade" 
-            break
-        if "\\shops\\" in x: 
-            fileType = "ShopDef" 
-            break
-        if "\\simGameConstants\\" in x: 
-            fileType = "SimGameConstants" 
-            break
-        if "\\simGameConversations\\" in x: 
-            fileType = "SimGameConversations" 
-            break #.convo.bytes files
-        if "\\simGameStatDesc\\" in x: 
-            fileType = "SimGameStatDescDef" 
-            break
-        if "\\starsystem\\" in x: 
-            fileType = "StarSystemDef" 
-            break
-        if "\\turretChassis\\" in x: 
-            fileType = "TurretChassisDef" 
-            break
-        if "\\turrets\\" in x: 
-            fileType = "TurretDef" 
-            break
-        if "\\upgrades\\" in x: 
-            fileType = "UpgradeDef" 
-            break
-        if "\\vehicle\\" in x: 
-            fileType = "VehicleDef" 
-            break
-        if "\\vehicleChassis\\" in x: 
-            fileType = "VehicleChassisDef" 
-            break
-        if "\\weapon\\" in x: 
-            fileType = "WeaponDef" 
-            break
-        if "\\factions\\" in x: 
-            fileType = "Texture2D" 
-            break # need exception for .dds files here or it will break with FactionDef. Also needs to add line twice for Texture2D and Sprite if in emblems/player/ folder
-        if "\\assetbundles\\" in x: 
-            fileType = "AssetBundle" 
-            break
-        if "\\maps\\" in x: 
-            fileType = "LayerData" 
-            break # ...LayerData.bin files
-        if "\\maps\\" in x: 
-            fileType = "TerrainData" 
-            break # ...TerrainData.bin files
-    filePath = x
-    fileDate = str(datetime.datetime.now().isoformat()) + "Z"
-    fileDate = fileDate.replace(' ', '-')
-    x = "{0},{1},{2},8,{3},{3},,,False,0,False".format(fileID, fileType, filePath, fileDate)
-    vma = open('VersionManifest.csv','a')
-    vma.write(x)
+    current_files = createTree.generate_data_file_list(battletech_home)
 
-lastLine = "\n,"
-vma.write(lastLine)
-vma.close()
+    # not sure about how efficient this is, maybe a list comprehension could be faster?
+    added_files = [file for file in current_files if file not in default_files]
+    # something like added_files = [file for file in current_files if file not in default_files]
+    # Removes blanks lines : unsure if needed with list comprehension / refactored file list reading code
+    added_files = filter(None, added_files)
+    return added_files
+
+
+def get_new_manifest_lines(added_files):
+    new_lines = []
+    for added_file_path in added_files:
+        fileID = os.path.basename(added_file_path)
+        fileID = os.path.splitext(fileID)[0]
+        fileType = get_file_type(added_file_path)
+        fileDate = str(datetime.datetime.now().isoformat()) + "Z"
+        fileDate = fileDate.replace(' ', '-')
+        new_manifest_line = "{0},{1},{2},8,{3},{3},,,False,0,False".format(fileID, fileType, added_file_path, fileDate)
+        new_lines.append(new_manifest_line)
+    return new_lines
+
+
+def write_new_manifest(new_manifest_lines):
+    copy_original_manifest()
+    new_manifest = open('VersionManifest.csv', 'a')
+    for new_line in new_manifest_lines:
+        new_manifest.write(new_line)
+        new_manifest.write('\n')
+    new_manifest.write(',')
+    new_manifest.close()
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read('config.ini')
+    battletech_home = config.get('directories', 'battletech_home')
+    added_files = get_added_file_list()
+    new_manifest_lines = get_new_manifest_lines(added_files)
+    write_new_manifest(new_manifest_lines)
